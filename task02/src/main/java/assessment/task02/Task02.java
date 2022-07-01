a347f4dddc9e04e51d6000594440c95b049eb352
Neal Goh <nealgoh1996@gmail.com>
2022-07-01 16:51:57 +0800

    assessment01July

--- task02/src/main/java/assessment/task02/Task02.java
+++ task02/src/main/java/assessment/task02/Task02.java
@@ -1,13 +1,23 @@
 package assessment.task02;
 
-/**
- * Hello world!
- *
- */
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Task02 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+
+        String host = "task02.chuklee.com";
+        int port = 80;
+        Socket s = new Socket(host, port);
+
+        //ObjectOutputStream os = new ObjectOutputStream(s.getOutputStream());
+        //ObjectInputStream is = new ObjectInputStream(s.getInputStream());
+
+        os.close();
+
+        DataOutputStream  outputStream = new DataOutputStream(s.getOutputStream());  
+        System.out.println("Output Stream: "+ outputStream);
     }
 }
